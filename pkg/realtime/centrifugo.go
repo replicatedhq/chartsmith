1a6e37a2e0d19f86026ab3808b9bf835aeb8fbb7
Marc Campbell <marc.e.campbell@gmail.com>
2025-02-25 07:14:44 -0800

    return to local dev

--- pkg/realtime/centrifugo.go
+++ pkg/realtime/centrifugo.go
@@ -67,6 +67,7 @@ func sendMessage(channelName string, data map[string]interface{}) error {
 		log.Fatalf("Error creating request: %v", err)
 	}
 
+	fmt.Printf("sending with api key %s\n", apiKey)
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Authorization", "apikey "+apiKey)
 
